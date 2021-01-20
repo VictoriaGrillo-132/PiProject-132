5a8349f5c626436fe8a15bf10c9af5c3ed9785b4
KiwiLemons <26863753+KiwiLemons@users.noreply.github.com>
2021-01-20 12:15:49 -0800

    Basic window dimensions

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import tkinter
+#yes
+window = Tk()
+window.title("Ready Set Study!")
+WIDTH = 800
+HEIGHT = 400
+window.geometry("{}x{}".format(WIDTH, HEIGHT))
+window.mainloop()
