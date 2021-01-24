54127d8759e9cc03ccb53bafb1f7859726476a21
Josue Gaona <jdgvs142001@gmail.com>
2021-01-24 15:35:33 -0600

    Added answers to the each section, and right and wrong outputs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 #import Rpi.GPIO as GPIO
-from time import sleep
 from random import randint
 Letters=("Times New Roman", 14)
 
@@ -57,19 +56,24 @@ class TypeQuestion(Frame):
     right_ans=["r","t","y","u","z"]
     def __init__(self, parent, question, location):
         Frame.__init__(self, parent)
+
+        TypeQuestion.the_que=question
         TypeQuestion.the__answer=location[1]
 
-        TypeQuestion.Que = Text(self, bg="light grey", height=2, font=Letters)
+        #Question Box
+        TypeQuestion.Que = Text(self, bg="light grey", height=3, font=Letters)
         TypeQuestion.Que.insert("1.0", question)
         TypeQuestion.Que.pack(anchor=N, fill=X)
         TypeQuestion.Que.config(state=DISABLED)
 
+        #Entry for Answering
         TypeQuestion.Ans = Entry(self, font=Letters)
         TypeQuestion.Ans.bind("<Return>", self.process)
-        TypeQuestion.Ans.pack(anchor=S, fill=X)
+        TypeQuestion.Ans.pack(anchor=S, fill=X, side=BOTTOM)
         TypeQuestion.Ans.focus()
 
-        TypeQuestion.back = Button(self, text="Back", command=self.Leave)
+        #Back Button
+        TypeQuestion.back = Button(self, text="Back", command=lambda Exit="nothing": self.Leave(Exit))
         TypeQuestion.back.pack(anchor=SW, expand=1)
 
     def process(self, event):
@@ -82,30 +86,38 @@ class TypeQuestion(Frame):
             self.Wrong()
 
         TypeQuestion.Ans.delete(0, END)       
-        '''
-        if TypeQuestion.location[1] == 0:
-            if response == TypeQuestion.right_ans
-        elif TypeQuestion.location[1] == 1:
-        elif TypeQuestion.location[1] == 2:
-        elif TypeQuestion.location[1] == 3:
-        elif TypeQuestion.location[1] == 4:
-            '''
+
     def Right(self):
         print("Right")
-        self.Leave()
+        self.Leave("Right")
 
     def Wrong(self):
         print("Wrong")
+        TypeQuestion.Que.config(state=NORMAL)
+        TypeQuestion.Que.delete("1.0",END)
+        TypeQuestion.Que.insert(END, TypeQuestion.the_que + "\nWrong")
+        TypeQuestion.Que.config(state=DISABLED)
 
-    def Leave(self):
-        self.pack_forget()
-        GBoard.pack(expand=1, fill=BOTH)
+    def Leave(self, Exit):
+        if Exit == "nothing":
+            self.pack_forget()
+            GBoard.pack(expand=1, fill=BOTH)
+            JepdyBoard.Que.delete("1.0",END)
+        else:
+            self.pack_forget()
+            GBoard.pack(expand=1, fill=BOTH)
+            JepdyBoard.Que.config(state=NORMAL)
+            JepdyBoard.Que.delete("1.0",END)
+            JepdyBoard.Que.insert(END, "Right")
+            JepdyBoard.Que.config(state=DISABLED)
 
 class TypeMulti(Frame):
     right_ans=["V","W","X","Y","Z"]
     wrong_ans=[["A","B","C"],["D","E","F"],["G","H","I"],["J","K","L"],["M","N","O"]]
     def __init__(self, parent, question, location):
         Frame.__init__(self, parent)
+
+        TypeMulti.the_que=question
 
         #Question Box
         TypeMulti.Que = Text(self, bg="light grey", width=70, height=3, font=Letters)
@@ -136,13 +148,72 @@ class TypeMulti(Frame):
         self.pack(expand=1, fill=BOTH)
 
         #Back Button
-        TypeMulti.back = Button(self, text="Back", command=self.Leave)
+        TypeMulti.back = Button(self, text="Back", command=lambda Exit="nothing": TypeMulti.Leave(self, Exit))
         TypeMulti.back.grid(row = 3, column=0, sticky=N+W+S)
 
     def Right(self):
         print("Right")
-        self.Leave()
+        self.Leave("Right")
     
+    def Wrong(self):
+        #print("Wrong")
+        TypeMulti.Que.config(state=NORMAL)
+        TypeMulti.Que.delete("1.0",END)
+        TypeMulti.Que.insert(END, TypeMulti.the_que + "\nWrong")
+        TypeMulti.Que.config(state=DISABLED)
+
+
+    def Leave(self, Exit):
+        if Exit == "nothing":
+            self.pack_forget()
+            GBoard.pack(expand=1, fill=BOTH)
+            JepdyBoard.Que.delete("1.0",END)
+        else:
+            self.pack_forget()
+            GBoard.pack(expand=1, fill=BOTH)
+            JepdyBoard.Que.config(state=NORMAL)
+            JepdyBoard.Que.delete("1.0",END)
+            JepdyBoard.Que.insert(END, "Right")
+            JepdyBoard.Que.config(state=DISABLED)
+
+#Just here for when we come up with 3rd game
+class TypeGuess(Frame):
+    right_ans=["ras","tes","yrs","uts","zvd"]
+    def __init__(self, parent, question, location):
+        Frame.__init__(self, parent)
+        TypeQuestion.the__answer=location[1]
+
+        #Question Box
+        TypeGuess.Que = Text(self, bg="light grey", height=2, font=Letters)
+        TypeGuess.Que.insert("1.0", question)
+        TypeGuess.Que.pack(anchor=N, fill=X)
+        TypeGuess.Que.config(state=DISABLED)
+
+        #Entry for Answering
+        TypeGuess.Ans = Entry(self, font=Letters)
+        TypeGuess.Ans.bind("<Return>", self.process)
+        TypeGuess.Ans.pack(anchor=S, fill=X, side=BOTTOM)
+        TypeGuess.Ans.focus()
+
+        #Back Button
+        TypeGuess.back = Button(self, text="Back", command=self.Leave)
+        TypeGuess.back.pack(anchor=SW, expand=1)
+
+    def process(self, event):
+        pass
+        response = TypeGuess.Ans.get()
+        response = response.lower()
+
+        if response == TypeGuess.right_ans[TypeQuestion.the__answer]:
+            self.Right()
+        else:
+            self.Wrong()
+
+        TypeQuestion.Ans.delete(0, END)       
+
+    def Right(self):
+        print("Right")
+
     def Wrong(self):
         print("Wrong")
 
@@ -150,8 +221,10 @@ class TypeMulti(Frame):
         self.pack_forget()
         GBoard.pack(expand=1, fill=BOTH)
 
-
 class JepdyBoard(Frame):
+    #current player
+    #cp=ValsandAns.ChoosePerson()
+    
     def __init__(self, parent):
         Frame.__init__(self, parent)
 
@@ -162,7 +235,7 @@ class JepdyBoard(Frame):
             #number of columns
             for col_index in range(5):
                 Grid.columnconfigure(self, col_index, weight=1)
-                btn = Button(self, command=lambda location=((row_index-2),col_index): JepdyBoard.BtnClick(self, location), text=f"${200*(row_index+1)}")
+                btn = Button(self, command=lambda location=((row_index-2),col_index): JepdyBoard.BtnClick(self, location), text=f"${300*(row_index-1)}")
                 if row_index == 2:
                     btn.grid(row=row_index, column=col_index, sticky=N+S+E+W, padx=10, pady=(20, 10))
                 else:
@@ -183,11 +256,16 @@ class JepdyBoard(Frame):
             QFrame.pack(expand=1, fill=BOTH)
         
         elif location[0] == 0:
-            QFrame = TypeMulti(window, ValsandAns.questions[location[0]][location[1]], location)
-            QFrame.pack(expand=1, fill=BOTH)
+            MFrame = TypeMulti(window, ValsandAns.questions[location[0]][location[1]], location)
+            MFrame.pack(expand=1, fill=BOTH)
+
+        elif location[0] == 2:
+            GFrame = TypeMulti(window, ValsandAns.questions[location[0]][location[1]], location)
+            GFrame.pack(expand=1, fill=BOTH)
+
             
         #print question in top texbox
-        ValsandAns.Questions(self, location)
+        #ValsandAns.Questions(self, location)
 
 window = Tk()
 window.title("Ready Set Study!")
